425109fd234298e502efd03e46446e0ff99249c9
Toni  Diep <adiep123@gmail.com>
2020-05-19 14:33:05 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
-  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+  @@all = []
+  def initialize(name, mood)
+    @name = name
+    @mood = mood
+    @mood = "nervous" 
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
-  # code goes here
+  attr_accessor :pets, :dogs, :cats
+  attr_reader :name, :species
+  @@all = []
+  @@pets = {:dogs => [], :cats => []}
+  def initialize(name, species = "human")
+    @name = name
+    @species = "human"
+    @species = species
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  def say_species
+    "I am a #{species}."
+  end
+  
+  
 end
